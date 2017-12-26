24b55ea91e022766af55518aee3289b98ab3faa8
Sherry El Garhy <sherryptk@gmail.com>
2017-12-26 22:33:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,11 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = album
+  end
 
   def self.create_table
       sql =  <<-SQL
