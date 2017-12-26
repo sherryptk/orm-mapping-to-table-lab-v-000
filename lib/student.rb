eefa3515982c65c5a2b3b6eb85e64ad21d6cf531
Sherry El Garhy <sherryptk@gmail.com>
2017-12-26 22:32:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  self.create_table
+  def self.create_table
       sql =  <<-SQL
         CREATE TABLE IF NOT EXISTS songs (
           id INTEGER PRIMARY KEY,
